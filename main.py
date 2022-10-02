4a998ff4d09b9c5387bccccb70df26d12acf59ec
jsk11235 <kirmayerjacob@gmail.com>
2022-10-02 00:48:49 -0400

    a

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
 import schemdraw
 import schemdraw.elements as elm
 
+
 def build_circut(drawing: schemdraw.Drawing, collector, statement, length):
     total_paren = 0
     split_idx = 0
+    operator = ''
     print(statement)
-    for i in range(1, len(statement) - 1):
-        if statement[i] == '(':
+    is_enclosed = statement[0] == '('
+    print(is_enclosed)
+    if is_enclosed:
+        start = 1
+        end = len(statement) - 1
+    else:
+        start = 0
+        end = len(statement)
+    for i in range(start, end):
+        c = statement[i]
+        if c == '(':
             total_paren += 1
-        elif statement[i] == ')':
+        elif c == ')':
             total_paren -= 1
-        elif total_paren == 0:
+        elif total_paren == 0 and (c == '&' or c == '!' or c == '|'):
             operator = statement[i]
             split_idx = i
             break
+    if len(operator) == 0:
+        operator = statement
 
-    s1 = statement[1:split_idx]
-    s2 = statement[split_idx + 1:-1]
+    s1 = statement[start:split_idx]
+    s2 = statement[split_idx + 1:end]
 
     if operator == '&':
         l1 = drawing.add(elm.Line().down(length).at(collector))
-        em1 = build_circut(drawing, l1.end, s1, length)
+        em1 = build_circut(drawing, l1.end, s1, length / 2)
         l2 = drawing.add(elm.Line().down(length).at(em1))
-        em2 = build_circut(drawing, l2.end, s2, length)
+        em2 = build_circut(drawing, l2.end, s2, length / 2)
         l3 = drawing.add(elm.Line().down(length).at(em2))
         emitter = l3.end
     elif operator == '|':
         l1 = drawing.add(elm.Line().down(length).at(collector))
-        l2 = drawing.add(elm.Line().right().at(l1.end))
+        l2 = drawing.add(elm.Line().right(4*length).at(l1.end))
         l3a = drawing.add(elm.Line().down(length).at(l2.end))
         l3b = drawing.add(elm.Line().down(length).at(l1.end))
-        em1 = build_circut(drawing, l3a.end, s1, length)
-        em2 = build_circut(drawing, l3b.end, s2, length)
+        em1 = build_circut(drawing, l3a.end, s1, length / 2)
+        em2 = build_circut(drawing, l3b.end, s2, length / 2)
         l4 = drawing.add(elm.Line().at(em2).to(em1))
         emitter = l4.end
     elif operator == '!':
         t = drawing.add(elm.BjtNpn(circle=True).right().anchor('collector').at(collector))
         emitter = t.emitter
-        l1 = drawing.add(elm.Line().left(length).at(t.base))
         drawing.add(elm.Resistor().left().at(t.base))
         l2 = drawing.add(elm.Line().up(length))
         drawing.add(elm.Resistor().up().at(l2.end))
         drawing.add(elm.Label().label('+'))
         l3 = drawing.add(elm.Line().down(length / 2).at(l2.start))
-        em = build_circut(drawing, l3.end, s2, length)
-        drawing.add(elm.Line().down(length/2).at(em))
+        em = build_circut(drawing, l3.end, s2, length / 2)
+        drawing.add(elm.Line().down(length / 2).at(em))
         drawing.add(elm.Label().label('-'))
     else:
         t = drawing.add(elm.BjtNpn(circle=True).right().anchor('collector').at(collector).label(operator))
@@ -52,10 +64,14 @@ def build_circut(drawing: schemdraw.Drawing, collector, statement, length):
     return emitter
 
 
-with schemdraw.Drawing() as draw:
-    line = draw.add(elm.Line().down(0.0001 * draw.unit))
-    draw.add(elm.Resistor().up().at(line.start))
-    draw.add(elm.Label().label('+'))
-    em = build_circut(draw, line.end, "((A)|(!(B)))", draw.unit * 0.3)
-    draw.add(elm.LED().down().at(em))
-    draw.add(elm.Line().down(0.0001 * draw.unit).label('-'))
+def full_build(logic):
+    with schemdraw.Drawing() as draw:
+        line = draw.add(elm.Line().down(0.0001 * draw.unit))
+        draw.add(elm.Resistor().up().at(line.start))
+        draw.add(elm.Label().label('+'))
+        em = build_circut(draw, line.end, "("+logic+")", draw.unit * 0.3)
+        draw.add(elm.LED().down().at(em))
+        draw.add(elm.Line().down(0.0001 * draw.unit).label('-'))
+
+
+full_build("(A|B)&(!(A&B))")
